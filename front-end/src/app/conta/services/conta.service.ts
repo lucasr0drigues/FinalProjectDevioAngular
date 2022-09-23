2c43631fbf09fe000a253a152d43577b4ee21edf
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-22 21:35:36 -0300

    add base service

--- front-end/src/app/conta/services/conta.service.ts
+++ front-end/src/app/conta/services/conta.service.ts
@@ -1,12 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { catchError,map } from "rxjs/operators";
+import { BaseService } from "src/app/services/base.service";
+import { Usuario } from "../models/usuario";
 
 @Injectable()
-export class ContaService{
-    constructor(private http: HttpClient){
-        
-        registrarUsuario(usuario: Usuario){
+export class ContaService extends BaseService{
 
+    constructor(private http: HttpClient){ super(); }
+        
+        registrarUsuario(usuario: Usuario) : Observable<Usuario>{
+            let response = this.http
+                .post(this.UrlServicev1 + 'nova-conta', usuario, this.ObterHeaderJson())
+                .pipe(
+                    map(this.extractData),
+                    catchError(this.serviceError))
+
+            return response;
         }
 
         login(usuario: Usuario){
